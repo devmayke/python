116777e0debbba1f4324e0b53cfc2f719a1005c0
AlunoTI <AlunoTI@estacio.acad>
2021-09-30 20:21:23 -0300

    Exercício pronto

--- python.py
+++ python.py
@@ -1,1 +1,51 @@
-print("funcionou no codesphere")
+# sexo = input("Digite o sexo: ")
+# if sexo == 'f':
+#     print('F - Feminino')
+# elif sexo == 'm':
+#     print('M - Musculino')
+# else:
+#     print('Sexo inválido')
+
+#####################################################################################
+
+# nota1 = float(input('nota 1: '))
+# nota2 = float(input('nota 2: '))
+# media = float((nota1+nota2)/2) 
+
+# if media >= 7 and media < 10:
+#     print('Aprovado')
+# elif media < 7:
+#     print('Reprovado')
+# else:
+#     print('Aprovado com distinção')
+
+#####################################################################################
+
+# n1= float(input('numero 1: '))
+# n2 = float(input('nnumero 2: '))
+# n3 = float(input('numero 3: '))
+# maior=n1
+# menor=n1
+# array = [n1, n2, n3]
+# for i in range(3):
+#     if array[i] > maior:
+#         maior = array[i]
+#     if array[i] < menor:
+#         menor = array[i]
+# print("maior", maior)
+# print("menor", menor)
+
+#####################################################################################
+
+# for i in range(50):
+#  if i%2 != 0:
+#    print(i)
+
+#####################################################################################
+
+# for i in range(1000):
+#   n = int(input("Digite um número entre 0 e 10: "))
+#   if n > 9 or n < 1:
+#     print("numero inválido")
+#   else:
+#     break
