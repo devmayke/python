33bfc58b19b770c63457ac124e11e373e81d03fa
Mayke <devmayke@gmail.com>
2021-09-30 21:27:19 -0300

    .

---
+++ exercicio.py
@@ -0,0 +1,22 @@
+# str1 = input("1 - ")
+# str2 = input("2 - ")
+# print("Conteúdo: ", str1, " ----- Comprimento: ", len(str1))
+# print("Conteúdo: ", str2, " ----- Comprimento: ", len(str2))
+# if str1 == str2:
+#   print("As strings possuem o mesmo conteúdo")
+# else:
+#   print("As strings não possuem o mesmo conteúdo")
+# if len(str1) == len(str2):
+#   print("As strings possuem o mesmo comprimento")
+# else:
+#   print("As strings não possem o mesmo comprimento")
+
+
+str3 = str(input("Digite seu nome: "))
+# for i in str3:
+#   print(i)
+for i in str3:
+  print(i)
+  for j in :
+    print(i, j)
+ 
--- python.py
+++ python.py
@@ -22,7 +22,7 @@
 #####################################################################################
 
 # n1= float(input('numero 1: '))
-# n2 = float(input('nnumero 2: '))
+# n2 = float(input('numero 2: '))
 # n3 = float(input('numero 3: '))
 # maior=n1
 # menor=n1
@@ -43,9 +43,10 @@
 
 #####################################################################################
 
-# for i in range(1000):
+
+
+# n =11
+# while n > 9 or n < 1:
 #   n = int(input("Digite um número entre 0 e 10: "))
 #   if n > 9 or n < 1:
-#     print("numero inválido")
-#   else:
-#     break
+#     print("Número inválido!")
